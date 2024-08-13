4f387c2dfa393ada0a544183e41b5dcf94f3ee48
Mr. Will <mr.will.com@outlook.com>
2024-08-13 20:51:14 +0800

    Fix stars today only includes the number before comma

--- src/lib/getTrending.js
+++ src/lib/getTrending.js
@@ -52,7 +52,8 @@ const getTrending = async () => {
         el
           .querySelector('span.float-sm-right')
           .textContent.trim()
-          .replace(' stars today', ''),
+          .replace(' stars today', '')
+          .replace(',', ''),
         10,
       )
 
